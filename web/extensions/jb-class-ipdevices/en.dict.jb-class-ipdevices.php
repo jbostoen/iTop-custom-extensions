311e77400b487ce8b6ff4544ded875fdc99f04fc
jbostoen <jeffrey.bostoen@izegem.be>
2017-02-17 15:51:37 +0100

    Build 2017.02.17-1549
    - makes abstract class IPDevice inherit from NetworkDevice instead of
    ConnectableCI.
    - despite incorrect in datamodel, it makes more sense to link both
    network devices and end user devices.
    - minor alteration of Network Device: device type can now be NULL.

--- web/extensions/jb-class-ipdevices/en.dict.jb-class-ipdevices.php
+++ web/extensions/jb-class-ipdevices/en.dict.jb-class-ipdevices.php
@@ -13,31 +13,43 @@
 
 Dict::Add('EN US', 'English', 'English', array(
 
-
+	//
+	// Class: IP Device (abstract)
+	//
 	'Class:IPDevice' => 'IP Device CI',
 	'Class:IPDevice+' => '', 
 	'Class:IPDevice/Attribute:management_ip' => 'Management IP',
-	'Class:IPDevice/Attribute:management_ip+' => '', 
+	'Class:IPDevice/Attribute:management_ip+' => '',  
 	
-	
+	//
+	// Class: IP Camera 
+	//
 	'Class:IPCamera' => 'IP Camera',
 	'Class:IPCamera+' => '',   
 	 
+	//
+	// Class: IP Device Generic
 	'Class:IPDeviceGeneric' => 'IP Device',
 	'Class:IPDeviceGeneric+' => '',   
 	
+	// Class: IP Phone
+	//
 	'Class:IPPhone' => 'IP Phone',
 	'Class:IPPhone+' => '',
 	'Class:IPPhone/Attribute:number_internal' => 'Internal number',
 	'Class:IPPhone/Attribute:number_internal+' => '', 
 	'Class:IPPhone/Attribute:number_external' => 'External number',
 	
-	
+	//
+	// Class: Model
+	//
 	'Class:Model/Attribute:type/Value:IPDeviceGeneric' => 'IP Device',
 	'Class:Model/Attribute:type/Value:IPDeviceGeneric+' => 'IP Device',
 	'Class:Model/Attribute:type/Value:IPCamera' => 'IP Camera',
 	'Class:Model/Attribute:type/Value:IPCamera+' => 'IP Camera',
 	
+	 
+
 ));
 
 
