8c8e02ddd2824f6f2921e4eb4c7264af3b453cbe
jbostoen <jeffrey.bostoen@izegem.be>
2018-03-24 13:46:28 +0100

    Version 20180324-1345
    * Work in progress: new extension to keep track of interventions. MakeRef() not working.

---
+++ wip/extensions/jb-class-intervention/en.dict.jb-class-intervention.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2013 XXXXX
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('EN US', 'English', 'English', array(
+	// Dictionary entries go here
+	'Class:Intervention' => 'Intervention',
+	'Class:Intervention+' => 'Logs an intervention.',
+	
+	
+	'Class:Intervention/Attribute:ref' => 'Reference',
+	'Class:Intervention/Attribute:ref+' => 'The reference for this intervention',
+	
+	'Class:Intervention/Attribute:agent_id' => 'Agent ID',
+	'Class:Intervention/Attribute:agent_id+' => 'The agent who did the intervention',
+	'Class:Intervention/Attribute:agent_name' => 'Agent name',
+	'Class:Intervention/Attribute:agent_name+' => 'The agent who did the intervention',
+	
+	'Class:Intervention/Attribute:org_id' => 'Client organization ID',
+	'Class:Intervention/Attribute:org_id+' => 'The organization for which you did the intervention',
+	'Class:Intervention/Attribute:org_name' => 'Client organization ID',
+	'Class:Intervention/Attribute:org_name+' => 'The organization for which you did the intervention',
+	
+	
+	'Class:Intervention/Attribute:start' => 'Start time',
+	'Class:Intervention/Attribute:start+' => 'The intervention started at ...',
+	
+	'Class:Intervention/Attribute:end' => 'End time',
+	'Class:Intervention/Attribute:end+' => 'The intervention ended at ...',
+	
+	'Class:Intervention/Attribute:type' => 'Type',
+	'Class:Intervention/Attribute:type/Value:on_site' => 'On-site',
+	'Class:Intervention/Attribute:type/Value:remote' => 'Remote', 
+	  
+	'Class:Intervention/Attribute:description' => 'Description',
+	'Class:Intervention/Attribute:description+' => 'Description of the intervention',
+	
+	'Menu:SearchInterventions' => 'Search for interventions',
+	'Menu:SearchInterventions+' => 'Search for interventions',
+	'Menu:NewIntervention' => 'New intervention',
+	'Menu:NewIntervention+' => 'Create a new intervention',
+));
+
+ 
+
+
+
+?>
---
+++ wip/extensions/jb-class-intervention/module.jb-class-intervention.php
@@ -0,0 +1,52 @@
+<?php
+//
+// iTop module definition file
+//
+
+SetupWebPage::AddModule(
+	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
+	'jb-class-intervention/2.4.180215',
+	array(
+		// Identification
+		//
+		'label' => 'Class: Intervention - adds Intervention',
+		'category' => 'business',
+
+		// Setup
+		//
+		'dependencies' => array(
+			'itop-request-mgmt-itil/2.4.0' 
+		),
+		'mandatory' => false,
+		'visible' => true,
+
+		// Components
+		//
+		'datamodel' => array(
+			'model.jb-class-intervention.php'
+		),
+		'webservice' => array(
+			
+		),
+		'data.struct' => array(
+			// add your 'structure' definition XML files here,
+		),
+		'data.sample' => array(
+			// add your sample data XML files here,
+		),
+		
+		// Documentation
+		//
+		'doc.manual_setup' => '', // hyperlink to manual setup documentation, if any
+		'doc.more_information' => '', // hyperlink to more information, if any 
+
+		// Default settings
+		//
+		'settings' => array(
+			// Module specific settings go here, if any
+		),
+	)
+);
+
+
+?>
