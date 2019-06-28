d8f6a24072cd898e76b0eec4e531d5e455342b53
jbostoen <jbostoen2@intranet.izegem.be>
2019-06-28 10:07:51 +0200

    Version 20190628-1006
    * jb-geom: fix OpenStreetMap center (actions menu)

--- wip/extensions/jb-geom/js/geometry-actions.js
+++ wip/extensions/jb-geom/js/geometry-actions.js
@@ -4,9 +4,10 @@
 		// OpenStreetMap expects EPSG:4326. This would only work for Point features. Adapt for other features.
 		// This function is included only in a page where 'geom' has already been set.
 		var oView = geometryHandler.oMap.getView();
-		var aCoords = geometryHandler.oFeature.clone().getGeometry().transform(oView.getProjection(), 'EPSG:4326');
-		document.location = 'https://www.openstreetmap.org/#map=' + oView.getZoom() + '/' + aCoords[1] + '/' + aCoords[0];		
-		
+		var aCoords = ol.proj.transform(geometryHandler.oCenter, oView.getProjection(), 'EPSG:4326');
+		var sUrl = 'https://www.openstreetmap.org/#map=' + oView.getZoom() + '/' + aCoords[1] + '/' + aCoords[0];
+		console.log(sUrl);
+		document.location = sUrl;
 	}
 	
 	function geometryHandler_Copy_As_GeoJSON(){
