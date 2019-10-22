40d0b37d5413ce6704657f75db9ca92c991613cb
jbostoen <jbostoen2@intranet.izegem.be>
2019-10-22 16:49:34 +0200

    Version 20191022-1649
    * fix jb-mail-to-ticket-automation

--- web/extensions/jb-mail-to-ticket-automation/jb-itop-standard-email-synchro/core/ormcustomcaselog.class.inc.php
+++ web/extensions/jb-mail-to-ticket-automation/jb-itop-standard-email-synchro/core/ormcustomcaselog.class.inc.php
@@ -3,155 +3,120 @@
 /**
  * @copyright   Copyright (C) 2019 Jeffrey Bostoen
  * @license     https://www.gnu.org/licenses/gpl-3.0.en.html
- * @version     2019-08-11 20:40:30
+ * @version     2019-08-22 12:47:48
  *
- * Custom version of ormCaseLog.
- * Extended AddLogEntry() to support on_behalf_of_user_id (rather than just 'on_behalf_of'). 
- * Important: NOT implemented so for AddLogEntryFromJSON(). If needed, add this.
- *
- * Originally planned an UpdateLogEntryAt() to support retro-active updating (meant for custom version of 'Mail to Ticket Automation'); 
- * but the method would have to loop over each entry every time anyway.
- * Therefore, it's better to build an ormCustomCaseLog from scratch and use AddLogEntry()
+ * Custom version of ormCaseLog. Likely to be included in my fork of Mail to Ticket Automation.
+ * extended AddLogEntry() to support on_behalf_of_user_id (rather than just 'on_behalf_of'). Important: NOT done so for AddLogEntryFromJSON(). If needed, do this.
+ * originally intended an UpdateLogEntryAt() to support retro-active updating (meant for our own version of 'Mail to Ticket Automation'); but it would have to loop each time anyway.
+ * therefore, it's better to build an ormCustomCaseLog from scratch and use AddLogEntry()
  */
  
-namespace jb_mail_to_ticket_automation;
 
-use \AttributeDateTime;
-use \ormCaseLog;
-use \HTMLSanitizer;	   
-use \UserRights;
+if( class_exists('ormCustomCaseLog') == false ) {
+	
 
-class ormCustomCaseLog extends ormCaseLog {
+	class ormCustomCaseLog extends ormCaseLog {
 
-	/**
-	 * Add a new entry to the log or merge the given text into the currently modified entry 
-	 * and updates the internal index
-	 * @param string $sText The text of the new entry
-	 * @param string $sOnBehalfOf  Custom specified user name (for example: "from:" in the Mail to Ticket Automation extension)
-	 * @param integer|null $iOnBehalfOfUserId Custom specified user ID
-	 * @param String $sDateTime Time sent
-	 */
-	public function AddLogEntry($sText, $sOnBehalfOf = '', $iOnBehalfOfUserId = null, $sDateTime = '')
-	{
-		$sText = HTMLSanitizer::Sanitize($sText);
-		$sDateTime = ($sDateTime == '' ? date(AttributeDateTime::GetInternalFormat()) : date(AttributeDateTime::GetInternalFormat(), strtotime($sDateTime)));
-		
-		$iUserId = null;
-		
-		if ($sOnBehalfOf == '')	{
-			// Not specified. Fall back to current user.
-			$sOnBehalfOf = UserRights::GetUserFriendlyName();
+		/**
+		 * Add a new entry to the log or merge the given text into the currently modified entry 
+		 * and updates the internal index
+		 * @param string $sText The text of the new entry
+		 * @param string $sOnBehalfOf  Custom specified user name (for example: "from:" in the Mail to Ticket Automation extension)
+		 * @param integer|null $iOnBehalfOfUserId Custom specified user ID
+		 * @param String $sDateTime Time sent
+		 */
+		public function AddLogEntry($sText, $sOnBehalfOf = '', $iOnBehalfOfUserId = null, $sDateTime = '')
+		{
+			$sText = HTMLSanitizer::Sanitize($sText);
+			$sDateTime = ($sDateTime == '' ? date(AttributeDateTime::GetInternalFormat()) : date(AttributeDateTime::GetInternalFormat(), strtotime($sDateTime)));
 			
-			if($iOnBehalfOfUserId === null) {
-				// Not specified. Fall back co current user's ID
+			if ($sOnBehalfOf == '')	{
+				$sOnBehalfOf = UserRights::GetUserFriendlyName();
 				$iUserId = UserRights::GetUserId();
 			}
-		}
-		
-		/* 
-			No prepending!
-			if ($this->m_bModified)
-			{
-				$aLatestEntry = end($this->m_aIndex);
-				if ($aLatestEntry['user_name'] == $sOnBehalfOf && $aLatestEntry['user_id'] == $iUserId)
-				{
-					// Append the new text to the previous one
-					$sPreviousText = substr($this->m_sLog, $aLatestEntry['separator_length'], $aLatestEntry['text_length']);
-					$sText = $sPreviousText."\n".$sText;
-
-					// Cleanup the previous entry
-					array_pop($this->m_aIndex);
-					$this->m_sLog = substr($this->m_sLog, $aLatestEntry['separator_length'] + $aLatestEntry['text_length']);
-				}
+			if( $iOnBehalfOfUserId !== null ) {
+				$iUserId = $iOnBehalfOfUserId;
 			}
-		*/
+			else {
+				$iUserId = 0;
+			}
+			
+			/* 
+				No prepending!
+				if ($this->m_bModified)
+				{
+					$aLatestEntry = end($this->m_aIndex);
+					if ($aLatestEntry['user_name'] == $sOnBehalfOf && $aLatestEntry['user_id'] == $iUserId)
+					{
+						// Append the new text to the previous one
+						$sPreviousText = substr($this->m_sLog, $aLatestEntry['separator_length'], $aLatestEntry['text_length']);
+						$sText = $sPreviousText."\n".$sText;
 
-		$sSeparator = sprintf(CASELOG_SEPARATOR, $sDateTime, $sOnBehalfOf, $$iOnBehalfOfUserId);
-		$iSepLength = strlen($sSeparator);
-		$iTextlength = strlen($sText);
-		
-		// Not looking to add duplicate entries, so
-		$aEntry =  array(
-			'user_name' => $sOnBehalfOf,
-			'user_id' => $$iOnBehalfOfUserId,
-			'date' => strtotime($sDateTime),
-			'text_length' => $iTextlength,
-			'separator_length' => $iSepLength,
-			'format' => 'html',
-		);
-		
-		if(in_array($aEntry, $this->m_aIndex) == false) {
-		
+						// Cleanup the previous entry
+						array_pop($this->m_aIndex);
+						$this->m_sLog = substr($this->m_sLog, $aLatestEntry['separator_length'] + $aLatestEntry['text_length']);
+					}
+				}
+			*/
+
+			$sSeparator = sprintf(CASELOG_SEPARATOR, $sDateTime, $sOnBehalfOf, $iUserId);
+			$iSepLength = strlen($sSeparator);
+			$iTextlength = strlen($sText);
 			$this->m_sLog = $sSeparator.$sText.$this->m_sLog; // Latest entry printed first
-			$this->m_aIndex[] = $aEntry;
+			$this->m_aIndex[] = array(
+				'user_name' => $sOnBehalfOf,
+				'user_id' => $iUserId,
+				'date' => strtotime($sDateTime),
+				'text_length' => $iTextlength,
+				'separator_length' => $iSepLength,
+				'format' => 'html',
+			);
 			$this->m_bModified = true;
+		}
+		
+		/**
+		 * Returns entries
+		 * 
+		 * @return Array
+		 */
+		public function GetEntries() {
+			return $this->m_aIndex;
+		}
+		
+		/**
+		 * Sorts case log entries by timestamp ('date')
+		 *
+		 * @param Boolean $bAscending Defaults to true.
+		 *
+		 * @return void
+		 *
+		 * @details Warning: if DBUpdate() is called AFTER this, it will be considered a modification and it will be likely be logged as 'new entry added'
+		 */
+		public function ToSortedCaseLog($bAscending = true) {
+			
+			$aEntries = $this->GetAsArray();
+			
+			usort($aEntries, function ($item1, $item2) use ($bAscending) {
+				
+				$dtCompare1 = strtotime($item1['date']);
+				$dtCompare2 = strtotime($item2['date']);
+				
+				return (($dtCompare1 <=> $dtCompare2) * ( $bAscending == true ? 1 : -1));
+			});
+						
+			// m_aIndex AND m_sLog both need to be updated, hence this trick.
+			$oCustomCaseLog = new ormCustomCaseLog();
+			
+			// Above logic is: order as requested. $aEntries contains the log entries in the wanted order.
+			// However, since adding happens on top, it needs to be reversed below.
+			foreach(array_reverse($aEntries) as $aEntry) {
+				$oCustomCaseLog->AddLogEntry($aEntry['message_html'], $aEntry['user_login'], $aEntry['user_id'], $aEntry['date']);
+			}
+			
+			return $oCustomCaseLog;
 			
 		}
 		
 	}
-	
-	/**
-	 * Adds case log entries from a provided source ormCaseLog
-	 *
-	 * @param ormCaseLog $oSourceCaseLog Case log
-	 *
-	 * @return void
-	 */
-	public function AddLogEntriesFromCaseLog($oSourceCaseLog) {
-		
-		foreach($oSourceCaseLog->GetAsArray() as $aEntry) {
-			
-			// CustomCaseLog's AddLogEntry remains flexible; keeps original user information and datetime
-			$this->AddLogEntry( $aEntry['message_html'], $aEntry['user_login'], $aEntry['user_id'], $aEntry['date'] );
-			
-		}
-		
-	}
-	
-	/**
-	 * Returns entries
-	 * 
-	 * @return Array
-	 */
-	public function GetEntries() {
-		return $this->m_aIndex;
-	}
-	
-	
-	
-	/**
-	 * Sorts case log entries by timestamp ('date')
-	 *
-	 * @param Boolean $bAscending Defaults to true.
-	 *
-	 * @return void
-	 *
-	 * @details Warning: if DBUpdate() is called AFTER this, it will be considered a modification and it will be likely be logged as 'new entry added'
-	 */
-	public function ToSortedCaseLog($bAscending = true) {
-		
-		$aEntries = $this->GetAsArray();
-		
-		usort($aEntries, function ($item1, $item2) use ($bAscending) {
-			
-			$dtCompare1 = strtotime($item1['date']);
-			$dtCompare2 = strtotime($item2['date']);
-			
-			return (($dtCompare1 <=> $dtCompare2) * ( $bAscending == true ? 1 : -1));
-		});
-		
-		// m_aIndex AND m_sLog both need to be updated, hence this trick.
-		$oCustomCaseLog = new \jb_mail_to_ticket_automation\ormCustomCaseLog();
-		
-		// The order above might be descending, as wanted.
-		// However, if that item gets added first, iTop will add the subsequent (older) issues on top of that entry again.
-		foreach(array_reverse($aEntries) as $aEntry) {
-			$oCustomCaseLog->AddLogEntry($aEntry['message_html'], $aEntry['user_login'], $aEntry['user_id'], $aEntry['date']);
-		}
-		
-		return $oCustomCaseLog;
-		
-	}
-	
+
 }
-	
