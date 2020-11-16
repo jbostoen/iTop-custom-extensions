c1e41f6b524ed7807072ec0f966ee7a0c8d53b5f
jbostoen <->
2020-11-16 11:14:04 +0100

    Version 2020-11-16 11:11:00
    * subrepos: fixes

--- utils/XAMPP/BAT Run iTop Cron.bat
+++
@@ -1,8 +0,0 @@
-REM copyright   Copyright (C) 2019-2020 Jeffrey Bostoen
-REM license     https://www.gnu.org/licenses/gpl-3.0.en.html
-REM version     2020-07-21 19:29:11
-
-:restart
-c:\xampp\php\php.exe c:\xampp\htdocs\itop\web\webservices\cron.php --auth_user=admin --auth_pwd=admin --verbose=1
-pause
-goto restart
